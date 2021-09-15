c087aee78b3503c98acf54f505290a6139c4d11e
vlAsTT <estadio7@gmail.com>
2021-09-15 17:50:26 +0300

    Set up of Game Scene

---
+++ Assets/Scripts/Spaceship/Interfaces/ISpaceshipCombat.cs
@@ -0,0 +1,11 @@
+namespace Core.Interfaces
+{
+    public interface ISpaceshipCombat
+    {
+        float Damage { get; set; }
+
+        void OnHit(float damage);
+
+        void Destroy();
+    }
+}
